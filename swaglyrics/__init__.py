3cc58c9d730ebb8c3edd70ce5fb9a846b88f2235
Srevin Saju <srevin03@gmail.com>
2020-06-27 15:40:13 +0300

    Remove unsupported_txt file path
    swaglyrics now depends on configuration manager, i.e
    swaglyrics.congig.config.CongigManager. this helps to
    prevent PermissionErrors and prevent the use of `sudo` on
    modern Linux Distribution

--- swaglyrics/__init__.py
+++ swaglyrics/__init__.py
@@ -1,10 +1,7 @@
-import os
-
 name = 'swaglyrics'
 __version__ = '1.2.0'
-unsupported_txt = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'unsupported.txt')
 backend_url = 'https://api.swaglyrics.dev'
 
 
 class SameSongPlaying(Exception):
-	pass
+    pass
